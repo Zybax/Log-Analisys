3ea9adf05374a907f9ad2c01e26e079587c64ae6
Oliver Lluberes <olimail47@gmail.com>
2018-01-29 09:11:17 -0500

    pep8

--- news.py
+++ news.py
@@ -1,54 +1,46 @@
 import psycopg2
 
+query1 = """select articles.slug, count(*) as views from log join articles
+            on log.path like '%'||articles.slug where log.status = '200 OK'
+            group by articles.slug order by views desc limit 3;"""
+
+query2 = """select authors.name, count(*) as views from authors join articles
+            on articles.author = authors.id join log
+            on log.path like '%'||articles.slug where log.status = '200 OK'
+            group by authors.name order by views desc;"""
+
+query3 = """select date, errorperc
+            from
+            (select date,  (
+                sum(views)/(select count(*)
+                from log where to_char( log.time,'MON,DD,YYYY' ) = date
+                ) * 100
+            ) as errorperc
+            from
+            (select to_char(log.time,'MON,DD,YYYY') as date, count(*) as views
+            from log where status != '200 OK' group by date ) as percentage
+            group by date order by errorperc desc )
+                    as result
+            where errorperc >= 1
+ """
+
 
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
     try:
         db = psycopg2.connect("dbname=news")
-        return db    
+        return db
     except expression as e:
         print e
-     
 
-def most_views():
+
+def command(query):
     results = []
     # Connect to the Database
     DB = connect()
     # Open a cursor to perform an operation
     cursor = DB.cursor()
     # Execute the Query
-    query = """ select articles.slug, count(log.path) as views from log join articles
-                on log.path like '%'||articles.slug group by articles.slug order by views desc limit 3
-            """
-    cursor.execute(query)
-    # Fecthing the results
-    data = cursor.fetchall()
-
-    for row in data:
-        results.append(row)
-    
-    DB.commit()
-    
-    # Close the cursor and the connection
-    cursor.close()
-    DB.close()
-    
-    #Output
-    for result in results:
-        print "{}-- {} views".format(result[0], result[1]) 
-
-
-def most_popular():
-    results = []
-    # Connect to the Database
-    DB = connect()
-    # Open a cursor to perform an operation
-    cursor = DB.cursor()
-    # Execute the Query
-    query = """ select authors.name, count(log.path) from authors join articles
-                on articles.author = authors.id join log 
-                on log.path like '%'||articles.slug group by authors.name order by count(log.path) desc 
-            """
     cursor.execute(query)
     # Fecthing the results
     data = cursor.fetchall()
@@ -57,47 +49,22 @@ def most_popular():
         results.append(row)
 
     DB.commit()
-    
+
     # Close the cursor and the connection
     cursor.close()
     DB.close()
 
-    for result in results:
-        print "{}-- {} views".format(result[0], result[1]) 
+    return results
 
+for result in command(query1):
+    print "{} -- {} views".format(result[0], result[1])
 
-def errors():
-    results = []
-    # Connect to the Database
-    DB = connect()
-    # Open a cursor to perform an operation
-    cursor = DB.cursor()
-    # Execute the Query
-    query = """
-            select time from log where status != '200 OK' limit 10
-            """
-    cursor.execute(query)
-    # Fecthing the results
-    data = cursor.fetchall()
+print "---------------------------------------"
 
-    for row in data:
-        results.append(row)
+for result in command(query2):
+    print "{} -- {} views".format(result[0], result[1])
 
-    DB.commit()
-    
-    # Close the cursor and the connection
-    cursor.close()
-    DB.close()
+print "---------------------------------------"
 
-
-    for result in results:
-        print "{} -- {}% errors".format(result[0], 0) 
-
-
-
-
-# most_views() 
-# print "---------------------------------------"
-# most_popular() 
-# print "---------------------------------------"
-errors()
+for result in command(query3):
+    print "{} -- {} % of errors".format(result[0], result[1])
